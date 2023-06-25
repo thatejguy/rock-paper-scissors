3ff8f365f1142bff118dc6d641b227256d7deea5
thatejguy <ejgrotto@gmail.com>
2023-06-25 22:24:08 +1000

    Added playround function

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ function getComputerChoice() {
     return randomValues;
 }
 
+//keeps track of scores
+let aPlayerScore = 0;
+let aComputerScore = 0;
+
+//plays one round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "Scissors") {
+        aPlayerScore += 1;
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+        aPlayerScore += 1;
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        aPlayerScore += 1;
+        return "You win! Scissors beats Paper!";
+    } else if (playerSelection === "rock" && computerSelection === "Paper") {
+        aComputerScore += 1;
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        aComputerScore += 1;
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        aComputerScore += 1;
+        return "You lose! Rock beats Scissors!";
+    } else {
+        aPlayerScore +=1;
+        aComputerScore += 1;
+        return "You tied!";
+    }
+}
+
